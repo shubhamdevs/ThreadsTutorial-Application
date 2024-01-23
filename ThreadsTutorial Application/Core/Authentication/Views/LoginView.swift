395d41b8360fbc3f87e9b29290fcd85c3bda3668
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-23 06:43:41 +0530

    build login page view

--- ThreadsTutorial Application/Core/Authentication/Views/LoginView.swift
+++ ThreadsTutorial Application/Core/Authentication/Views/LoginView.swift
@@ -8,8 +8,81 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image("threads-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                NavigationLink {
+                    Text("Forgert Password")
+                } label: {
+                    Text("Forget Password")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 20)
+                        .foregroundStyle(.black)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                })
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                    }
+                    .foregroundStyle(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
     }
 }
 
