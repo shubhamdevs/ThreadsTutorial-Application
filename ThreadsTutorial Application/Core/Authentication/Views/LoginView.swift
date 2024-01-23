2bba87a7b67a0bfcb63480d042fbd13572d8c28a
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-23 06:13:14 +0530

    add initial files to the threads app

---
+++ ThreadsTutorial Application/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsTutorial Application
+//
+//  Created by Shubham Biswas on 23/01/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
