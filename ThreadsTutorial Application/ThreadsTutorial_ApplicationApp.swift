fb63c5de37330371830ad574437ec44794001767
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-23 06:02:43 +0530

    Initial Commit

---
+++ ThreadsTutorial Application/ThreadsTutorial_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorial_ApplicationApp.swift
+//  ThreadsTutorial Application
+//
+//  Created by Shubham Biswas on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorial_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
